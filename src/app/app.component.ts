8506f919e0bcd778b8cf7cb575c270f8141d9a77
NonnoPinto <giovanni.zago.3@studenti.unipt.it>
2022-07-22 15:15:15 +0200

    Check connection on refresh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,52 +1,50 @@
-import { AfterViewInit, Component} from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import Web3 from 'web3';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements AfterViewInit{
 
+export class AppComponent implements AfterViewInit {
   title = 'metamask-balance-app';
   private web3: Web3 = new Web3(Web3.givenProvider);
 
   constructor() {}
 
-  access : any;
-  ethereumButton : any;
-  balanceButton : any;
-  balance : any;
+  access: any;
+  ethereumButton: any;
+  balanceButton: any;
+  balance: any;
   //_balance : any; Variabile usata per (eventualmente) salvare il bilancio in decimali
-  showAccount : any;
-  showBalance : any;
-  connected : boolean = true;
+  showAccount: any;
+  showBalance: any;
+  connected: boolean = true;
+
+  ngOnInit() {
+    //verifico se sono già collegato
+    this.checkConnection();
+  }
 
   ngAfterViewInit(): void {
     //Binding bottoni
     this.showBalance = document.querySelector('.showBalance');
     this.ethereumButton = document.querySelector('.enableEthereumButton');
+
     //Binding span
     this.showAccount = document.querySelector('.showAccount');
     this.balanceButton = document.querySelector('.showMyBalance');
-    //verifico se sono già collegato
-
     //event listener per i click sui bottoni
-    this.ethereumButton.addEventListener("click", () => {this.getAccount();});
-    this.balanceButton.addEventListener("click", () => {this.myGetBalance();});
+    this.ethereumButton.addEventListener('click', () => {
+      this.getAccount();
+    });
+    this.balanceButton.addEventListener('click', () => {
+      this.myGetBalance();
+    });
   }
 
-  //Vorrebbe in qualche modo verificare se si è già connessi all'apertura della pagina, senza dover cliccare sul bottone
-/*   checkConnection() : void {
-    this.accounts = this.web3.eth.getAccounts().then(console.log);
-    console.log("Lunghezza " + this.accounts.);
-    if (this.accounts.length > 0){
-      this.connected = false;
-      console.log("Ok, ho già un account");
-    }
-  } */
-
-  async getAccount(){
+  async getAccount() {
     //-->Indirizzo<--
     //Il metodo commentato segue il codice della libreria di MetaMask
     /* this.metaAccess= window;
@@ -56,16 +54,43 @@ export class AppComponent implements AfterViewInit{
     this.connected = false;
     this.access = this.access[0];
     this.showAccount.innerHTML = this.access;
-    console.log("Indirizzo " + this.access);
+    console.log('Indirizzo ' + this.access);
   }
 
-  async myGetBalance(){
+  async myGetBalance() {
     //-->Bilancio<--
     this.balance = await this.web3.eth.getBalance(this.access);
     //Conversione non indispensabile
     //this._balance = this.web3.utils.fromWei(this.balance, "ether");
-    this.showBalance.innerHTML = this.web3.utils.fromWei(this.balance, "ether");
-    console.log("Bilancio " + this.web3.utils.fromWei(this.balance, "ether"));
+    this.showBalance.innerHTML = this.web3.utils.fromWei(this.balance, 'ether');
+    console.log('Bilancio ' + this.web3.utils.fromWei(this.balance, 'ether'));
+  }
+
+  checkConnection() {
+    const handleAccountsChanged = this.handleAccountsChanged.bind(this) as () => void;
+    this.web3.eth.getAccounts().then(handleAccountsChanged);
+  }
+
+  async handleAccountsChanged(accounts) {
+    let currentAccount = null;
+
+    console.log(accounts);
+
+    //se non sono collegato, mostro il pulsante di login
+    if (accounts.length === 0) {
+      //console.log("Non sei connesso");
+      this.connected = true;
+    }
+    //altrimenti carico l'indirizzo
+    else {
+      currentAccount = accounts[0];
+      //console.log("Sei connesso");
+      this.connected = false;
+      //console.log(this.connected);
+      this.access = await this.web3.eth.requestAccounts();
+      this.access = this.access[0];
+      this.showAccount.innerHTML = this.access;
+    }
   }
 
 }
